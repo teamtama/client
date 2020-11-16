ce173036a2ad4c85bca00d492aa27ff47529de01
jonsoku2 <whdtjr2792@gmail.com>
2020-11-17 07:39:26 +0900

    Update firebase config

--- src/firebase/clientApp.ts
+++ src/firebase/clientApp.ts
@@ -1,33 +1,32 @@
-import firebase from 'firebase/app'
-import 'firebase/storage' // If you need it
-import 'firebase/analytics' // If you need it
-
-// const clientCredentials = {
-//   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-//   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-//   databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
-//   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-//   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-//   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-// }
+import firebase from "firebase/app";
+import "firebase/storage"; // If you need it
+import "firebase/analytics";
+import {
+  FIREBASE_API_KEY, FIREBASE_APP_ID,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_DATABASE_URL,
+  FIREBASE_MEASUREMENT_ID,
+  FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET
+} from "../../config"; // If you need it
 
 const clientCredentials = {
-  apiKey: 'AIzaSyDSVoXQZGv47Xx3C7yH6HyzmiV7SdZK7Yk',
-  authDomain: 'tamastudy-c49ef.firebaseapp.com',
-  databaseURL: 'https://tamastudy-c49ef.firebaseio.com',
-  projectId: 'tamastudy-c49ef',
-  storageBucket: 'tamastudy-c49ef.appspot.com',
-  messagingSenderId: '574711615042',
-  appId: '1:574711615042:web:0b530d857a08f41d69a7db',
-  measurementId: 'G-XD4G6RZR12',
-}
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  databaseURL: FIREBASE_DATABASE_URL,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
+  measurementId: FIREBASE_MEASUREMENT_ID
+};
 
 // Check that `window` is in scope for the analytics module!
-if (typeof window !== 'undefined' && !firebase.apps.length) {
-  firebase.initializeApp(clientCredentials)
+if (typeof window !== "undefined" && !firebase.apps.length) {
+  firebase.initializeApp(clientCredentials);
   // To enable analytics. https://firebase.google.com/docs/analytics/get-started
-  if ('measurementId' in clientCredentials) firebase.analytics()
+  if ("measurementId" in clientCredentials) firebase.analytics();
 }
 
-export default firebase
+export default firebase;
